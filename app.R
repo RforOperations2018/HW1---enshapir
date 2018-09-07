0d85654727beb787fec969526b6ce157a1579b52
Eric <enshapir@andrew.cmu.edu>
2018-09-07 16:30:29 -0400

    First Page Done
    Completed the first page of the app

---
+++ app.R
@@ -0,0 +1,75 @@
+#------------------------------
+#Eric Shapiro
+# R Shiny for Operations
+# Homework 1
+#------------------------------
+
+#libraies 
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+
+
+#infomartion on Data Used
+# 
+# ChickWeight
+# 
+# Format
+# 
+# An object of class c("nfnGroupedData", "nfGroupedData", "groupedData", "data.frame") containing the following columns:
+#   
+# weight - a numeric vector giving the body weight of the chick (gm).
+# Time - a numeric vector giving the number of days since birth when the measurement was made.
+# Chick - an ordered factor with levels 18 < ... < 48 giving a unique identifier for the chick. The ordering of the levels groups chicks on the same diet together and orders them according to their final weight (lightest to heaviest) within diet.
+# Diet - a factor with levels 1, ..., 4 indicating which experimental diet the chick received.
+
+
+Chicken.Weight <- ChickWeight
+
+DeitAverages <- Chicken.Weight %>% group_by(Diet) %>% summarise(Avg_Weight = mean(weight))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+  navbarPage("Chicken Deit Dashboard", 
+             theme = shinytheme("united"),
+             tabPanel("Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+                          numericInput(inputId = "ChickToDisplay", 
+                                       label = "Choose a Chick (18-48)",
+                                       value = 18, 
+                                       min = 18, 
+                                       max =48,
+                                       step = 1
+                          )
+                        ),
+                        # Output plot
+                        mainPanel(
+                          plotOutput(outputId = "Chickenplot")
+                        )
+                      )
+             ),
+             # Data Table
+             tabPanel("Table",
+                      fluidPage(DT::dataTableOutput("table"))
+             )
+  )
+)
+
+# Define server logic
+server <- function(input, output) {
+   
+  output$Chickenplot <- renderPlot({
+    #chickData <- filter(.data = Chicken.Weight, Chick == input$ChickToDisplay)
+    ggplot(data = Chicken.Weight, aes(x = Time, y = weight, color = Diet)) + geom_point()
+  })
+  output$DeitAverages <- renderPlot({
+    #chickData <- filter(.data = Chicken.Weight, Chick == input$ChickToDisplay)
+    ggplot(data = DeitAverages, aes(x = Diet, y = Avg_Weight, fill = Diet)) + geom_bar(stat = "identity")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
